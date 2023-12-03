aeed574746b677eeb589b2af447aad61546dccb0
Aminu 'Seun Joshua <34725212+seun-ja@users.noreply.github.com>
2023-12-03 04:13:24 +0100

    day 2 done, only part 1 :(

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
-use day_1::day_one::solve_day_1;
+use day_1::{day_one::solve_day_1, day_two::solve_day_2};
 
 fn main() {
     // Day One
     solve_day_1();
+
+    // Day Two
+    solve_day_2();
 }
